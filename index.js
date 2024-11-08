fa53068e7433754c6b027e9787ffba5be1260fbd
VU QUOC HUY <vqh8124@gmail.com>
2024-11-08 17:36:04 +0700

    D1_L3

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-let number_condition = 10;
+for (let i = 2; i < 10; i++) {
+  console.log("Bảng nhân " + i);
 
-if (number_condition % 2 == 0) {
-  console.log(number_condition + " là số chẵn");
-} else {
-  console.log(number_condition + " là số lẻ");
+  for (let j = 1; j <= 10; j++) {
+    console.log(i + " x " + j + " = " + i * j);
+    if (j == 10) {
+      console.log("----------------------");
+    }
+  }
 }
