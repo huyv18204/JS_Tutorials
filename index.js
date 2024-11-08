683304aee9639120d221ee19cefee9d2cda0007c
VU QUOC HUY <vqh8124@gmail.com>
2024-11-08 17:39:33 +0700

    D1_L4

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-for (let i = 2; i < 10; i++) {
-  console.log("Bảng nhân " + i);
-
-  for (let j = 1; j <= 10; j++) {
-    console.log(i + " x " + j + " = " + i * j);
-    if (j == 10) {
-      console.log("----------------------");
-    }
-  }
-}
+const array = [12, 5, 8, 19, 3, 22, 7];
+array.sort((a, b) => a - b);
+console.log(array);
