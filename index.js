83e9297dfc597136f1d94a367b67fd3f8f56f3a4
VU QUOC HUY <vqh8124@gmail.com>
2024-11-09 10:18:29 +0700

    D2_L3

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-function createCounter() {
-  let count = 0;
-  return function () {
-    count++;
-    return count;
-  };
-}
+fetch("https://jsonplaceholder.typicode.com/todos/1")
+  .then((response) => response.json())
+  .then((json) => console.log(json))
+  .catch((err) => console.log(err));
 
-const counter = createCounter();
+const fetchApi = async () => {
+  const url = "https://jsonplaceholder.typicode.com/todos/1";
+  const response = await fetch(url);
 
-console.log(counter());
-console.log(counter());
-console.log(counter());
+  try {
+    if (response.status !== 200) {
+      throw new Error("Đã xảy ra lỗi");
+    }
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error("Có lỗi xảy ra:", error);
+  }
+};
+
+fetchApi();
