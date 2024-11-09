d751c8ec90a6e38db2466e87e0a8892a93a5eab6
VU QUOC HUY <vqh8124@gmail.com>
2024-11-09 09:34:51 +0700

    fix

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+const sort = (arr) => {
+  let n = arr.length - 1;
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+};
+
 const array = [12, 5, 8, 19, 3, 22, 7];
-array.sort((a, b) => a - b);
-console.log(array);
+let sortArr = sort(array);
+console.log(sortArr);
